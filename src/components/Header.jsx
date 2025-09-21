e1d6432359b6e6fdae0f114cb36b945e82778c3c
Pomyuy2 <12em0te1231234@gmail.com>
2025-09-22 01:10:49 +0700

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>Header Component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro quisquam delectus quasi at et voluptatem eveniet explicabo consectetur vitae asperiores nisi, maiores, molestiae cupiditate modi nobis unde dolores sint.</p>
+    </header>
+  )
+}
+
+export default Header
